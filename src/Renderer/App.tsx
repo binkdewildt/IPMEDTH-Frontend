d98fdb18ea2f7e73872b45f66f1f6f70dcaeaf25
Bink <bink@famdewildt.nl>
2023-11-27 21:03:39 +0100

    Set the base for the router

--- src/Renderer/App.tsx
+++ src/Renderer/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<Router>
+		<Router basename="IPMEDTH-Frontend">
 			<Routes>
 				<Route index path="/" element={<div style={{width: "100px", height: "100px", background: "green"}}></div>} />
 				<Route path="/game" element={<div style={{width: "100px", height: "100px", background: "red"}}></div>} />
