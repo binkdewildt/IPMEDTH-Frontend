17a64d9eeb0406bfb4f85fe8bd3b4007d6a56c41
Bink <bink@famdewildt.nl>
2023-11-27 18:09:07 +0100

    Added the beginning to the router

--- src/Renderer/App.tsx
+++ src/Renderer/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Router>
+			<Routes>
+				<Route index path="/" element={<div style={{width: "100px", height: "100px", background: "green"}}></div>} />
+				<Route path="/game" element={<div style={{width: "100px", height: "100px", background: "red"}}></div>} />
+				<Route path="/end" element={<div style={{width: "100px", height: "100px", background: "orange"}}></div>} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
