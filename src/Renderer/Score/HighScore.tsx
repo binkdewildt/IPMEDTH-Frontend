46634fd43c73162d299209472fc6f9d077b2134f
Dishang <dishang04@gmail.com>
2023-11-29 15:14:51 +0100

    progress for high score page for ipad pro not responsive yet

--- src/Renderer/Score/HighScore.tsx
+++ src/Renderer/Score/HighScore.tsx
@@ -4,8 +4,19 @@ const HighScore: React.FC<{}> = () => {
   return (
     <p>
       <h1>Scores</h1>
-      <button>Speel</button>
-      <button>Terug</button>
+      <section>
+        <ol>
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+        </ol>
+      </section>
+      <button className="play">Speel</button>
+      <button className="prev">Terug</button>
+      {/* <div className="button-container">
+        <button>Speel</button>
+        <button>Terug</button>
+      </div> */}
     </p>
   );
 };
