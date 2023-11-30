564fdbf5f3c7a42158357705e2c4de8c35e08acb
Tim Roks <h.p.t.roks@gmail.com>
2023-11-30 15:40:52 +0100

    added maze framework

--- src/Renderer/Game/Game.tsx
+++ src/Renderer/Game/Game.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Game() {
-	return <div>Game</div>;
+    return (
+        <body className={"game"}>
+        <section className={"stage"}>
+            <image>hier komt het level</image>
+        </section>
+        <section className="mazeContainer">
+            <canvas id={"mazeCanvas"}>
+				hier komt het doolhof
+            </canvas>
+        </section>
+        </body>
+    );
 }
