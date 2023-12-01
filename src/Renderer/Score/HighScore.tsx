b0f9c34e5bcb9d0463e4641693becb6bb86b09b2
Dishang <dishang04@gmail.com>
2023-12-01 15:12:26 +0100

    added local storage

--- src/Renderer/Score/HighScore.tsx
+++ src/Renderer/Score/HighScore.tsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { useLocalStorage } from "../useLocalStorage";
 
 const HighScore: React.FC<{}> = () => {
+  const [state, setState] = useLocalStorage('test', 1000);
   return (
     <div className="grid-container">
       <h1>Scores</h1>
       <section>
         <ol>
-          <li>test score 1</li>
+          <li>{state}</li>
           <li>test score 2</li>
           <li>test score 3</li>
         </ol>
       </section>
-      <button className="play-button">Speel</button>
-      <button className="prev-button">Terug</button>
+      <button onClick={() => setState(Math.floor(Math.random() * 10000))} className="play-button">Speel</button>  {/*now button change the value state*/}
+      <button onClick={() => console.log("previous button pressed")} className="prev-button">Terug</button>
     </div>
   );
 };
