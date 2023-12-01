c58a3a5da0b3432c20aa1d8a2ac901e13fd4a907
JannadeVries <jannavries98@gmail.com>
2023-12-01 16:02:24 +0100

    first starting page

--- src/Renderer/Start/StartScreen.tsx
+++ src/Renderer/Start/StartScreen.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import startButton from "../../img/start-button.png"
 
 export default function StartScreen() {
 	return (
 		<section className="startScreen">
-			<Link className="link" to={"game"}> Start game </Link>
+			<h1 className="title">SINTERKLAASJOURNAAL</h1>
+			<button className="startButton" onClick={explanation}>
+				<img className="startImage" src={startButton} alt="klik voor de uitleg"/>
+			</button>
 		</section>
 	);
+}
+
+function explanation() {
+	<section className="explanation">
+		<Link className="link" to={"game"}> Start game </Link>
+	</section>
 }
