e1effb0517cbf74949a5edcd600b834c16ae5a1d
Bink de Wildt <41636464+binkdewildt@users.noreply.github.com>
2023-12-04 14:19:27 +0100

    Revert "Homepage"

--- src/Renderer/Start/StartScreen.tsx
+++ src/Renderer/Start/StartScreen.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import startButton from "../../img/start-button.png"
 
 export default function StartScreen() {
 	return (
 		<section className="startScreen">
-			<h1 className="title">SINTERKLAASJOURNAAL</h1>
-			<button className="startButton" onClick={explanation}>
-				<img className="startImage" src={startButton} alt="klik voor de uitleg"/>
-			</button>
+			<Link className="link" to={"game"}> Start game </Link>
 		</section>
 	);
-}
-
-function explanation() {
-	<section className="explanation">
-		<Link className="link" to={"game"}> Start game </Link>
-	</section>
 }
