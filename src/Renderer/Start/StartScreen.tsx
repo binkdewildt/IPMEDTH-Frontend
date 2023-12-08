2b73c1f5c0c43d523c630aa58365fe22a8b5f076
JannadeVries <jannavries98@gmail.com>
2023-12-08 14:45:24 +0100

    Basic startingpage

--- src/Renderer/Start/StartScreen.tsx
+++ src/Renderer/Start/StartScreen.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+//import { useState } from 'react';
 import { Link } from "react-router-dom";
+import startButton from "../../Assets/mediatrigger-button.png";
+import skjBanner from "../../Assets/banner.gif";
 
 export default function StartScreen() {
+	//todo: resize-animation startImg
 	return (
 		<section className="startScreen">
-			<Link className="link" to={"game"}> Start game </Link>
+			<img className="sinterklaasjournaalBanner" src={skjBanner} alt="Het bewegende logo van het Sinterklaasjournaal" height="307" width="642"></img>
+			<Link className="gameLink" to={"game"}> 
+				<img className="startImg" src={startButton} alt="Klik hier om de uitleg te starten" height="354" width="354"></img>
+			</Link>
 		</section>
 	);
 }
