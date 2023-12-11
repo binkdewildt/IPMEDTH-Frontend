22ef9a98d2cc90064d7eb1a7e2b1fdfa0ab82394
Bink <bink@famdewildt.nl>
2023-12-11 12:46:03 +0100

    Push

--- src/Renderer/Start/StartScreen.tsx
+++ src/Renderer/Start/StartScreen.tsx
@@ -6,7 +6,7 @@ import startButton from "../../Assets/mediatrigger-button.png";
 import skjBanner from "../../Assets/banner.gif";
 
 export default function StartScreen() {
-	const [isExplanationOpen, setIsExplanationOpen] = useState(true);
+	const [isExplanationOpen, setIsExplanationOpen] = useState(false);
 
 	//todo: resize-animation startImg
 	//todo: verplaats uitlegOverlay naar spel
