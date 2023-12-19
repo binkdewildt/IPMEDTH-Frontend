cd2bed29249a8696aa0685d34cb7b0a833e9318b
Bink de Wildt <bink099@gmail.com>
2023-12-19 09:42:54 +0100

    Fixed the end screen scss missing

--- src/Renderer/Score/HighScore.tsx
+++ src/Renderer/Score/HighScore.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { useLocalStorage } from "../../Hooks/useLocalStorage";
+import { Link } from "react-router-dom";
 
 const HighScore: React.FC<{}> = () => {
-  const [state, setState] = useLocalStorage('test', 1000);
-  return (
-    <div className="grid-container">
-      <h1>Scores</h1>
-      <section>
-        <ol>
-          <li>{state}</li>
-          <li>test score 2</li>
-          <li>test score 3</li>
-        </ol>
-      </section>
-      <button onClick={() => setState(Math.floor(Math.random() * 10000))} className="play-button">Speel</button>  {/*now button change the value state*/}
-      <button onClick={() => console.log("previous button pressed")} className="prev-button">Terug</button>
-    </div>
-  );
+	const [state] = useLocalStorage("test", 1000);
+  
+	return (
+		<div className="score-container">
+			<h1>Scores</h1>
+			<section>
+				<ol>
+					<li>{state}</li>
+					<li>test score 2</li>
+					<li>test score 3</li>
+				</ol>
+			</section>
+			<Link className="button-highScore button-highScore--play" to={"/game"}>
+				Speel
+			</Link>
+			<Link className="button-highScore button-highScore--prev" to={"/"}>
+				Terug
+			</Link>
+		</div>
+	);
 };
 
 export default HighScore;
