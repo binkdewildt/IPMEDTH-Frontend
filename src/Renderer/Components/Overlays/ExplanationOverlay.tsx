b09ff586bd3ad5b917226352e71236efd5695119
JannadeV <s1124226@student.hsleiden.nl>
2024-01-15 16:46:13 +0100

    responsive

--- src/Renderer/Components/Overlays/ExplanationOverlay.tsx
+++ src/Renderer/Components/Overlays/ExplanationOverlay.tsx
@@ -19,7 +19,8 @@ import stepSound from "../../../Assets/sounds/footsteps.mp3";
 import buttons from "../../../Assets/buttonsPicture.png";
 import cursor from "../../../Assets/cursorImage.png";
 
-	//todo: introduce navigation
+const isPhoneScreen = window.innerWidth < window.innerHeight;
+
 
 export function Overlay({ isOpen, onClose } : { isOpen: boolean, onClose: () => void }) {
 	return (
@@ -70,28 +71,31 @@ function Explanation() {
 			<div className="blinkBorder"></div>
 			<div className={`assetsContainer ${positionAssetsContainer}`}>
 				<Asset
+					umpteenth={0}
 					isActive={activeIndex===0}
 					textDiewertje={presentDiewertje}
 					sound={presentSound}
 					imgAlt="cadeautje"
 					imgSrc={present}
 					onShow={() => {
-						setPositionAssetsContainer(shiftAsset(activeIndex));
+						if (!isPhoneScreen) setPositionAssetsContainer(shiftAsset(activeIndex));
 						setActiveIndex(1);
 					}}
 				></Asset>
 				<Asset
+					umpteenth={1}
 					isActive={activeIndex===1}
 					textDiewertje={shoeDiewertje}
 					sound={shoeSound}
 					imgAlt="schoen"
 					imgSrc={shoe}
 					onShow={() => {
-						setPositionAssetsContainer(shiftAsset(activeIndex));
+						if (!isPhoneScreen) setPositionAssetsContainer(shiftAsset(activeIndex));
 						setActiveIndex(2);
 					}}
 				></Asset>
 				<Asset
+					umpteenth={2}
 					isActive={activeIndex===2}
 					textDiewertje={lightDiewertje}
 					sound={lightSound}
@@ -99,8 +103,8 @@ function Explanation() {
 					imgSrc={light}
 					onShow={() => {
 						setActiveIndex(3);
-						endAssets();
-						setMaze(true);;
+						if (!isPhoneScreen) endAssets();
+						setMaze(true);
 					}}
 				></Asset>
 			</div>
@@ -120,47 +124,43 @@ function MazeButtons() {
 function MazeExample() {
 	const errorSoundRef = useRef<HTMLAudioElement | null>(null);
 	const stepSoundRef = useRef<HTMLAudioElement | null>(null);
-	const [mazeExampleStyle, setMazeExampleStyle] = useState({
-		clipPath: "circle(calc(30vh / 2) at 10% 10%)",
-		left: "30%",
-		top: "30%"
-	});
+	const [mazeExampleStyle, setMazeExampleStyle] = useState( moveMaze(1,2)	);
 	const [pietState, setPietState] = useState(`${pietStanding}`)
 	useEffect(() => {
+		blinkSubjectBorder("0px");
 		setMazeExampleStyle(moveMaze(1,2));
 		moveCursor("");
-		blinkSubjectBorder("3px");
 		const timer0 = setTimeout(() => {
 			setPietState(`${pietHorizontal}`);
 			moveCursor("right");
-		}, 200);
+		}, 400);
 		const timer1 = setTimeout(() => {
 			setMazeExampleStyle(moveMaze(1.4, 2));
 			if(errorSoundRef.current !== null) {
 				errorSoundRef.current.play();
 			}
-		}, 400)
+		}, 600)
 		const timer2 = setTimeout(() => {
 			moveCursor("");
 			setPietState(`${pietLeft}`)
-		}, 1000);
+		}, 1100);
 		const timer3 = setTimeout(() => {
 			setMazeExampleStyle(moveMaze(1, 2));
 			setPietState(`${pietStanding}`);
-		}, 1600)
+		}, 1700)
 
 		const timer4 = setTimeout(() => {
 			moveCursor("down");
 			setPietState(`${pietDown}`);
-		}, 2400)
+		}, 2500)
 		const timer5 = setTimeout(() => {
 			setMazeExampleStyle(moveMaze(1, 2.5));
-		}, 3000)
+		}, 3100)
 		const timer6 = setTimeout(() => {
 			setMazeExampleStyle(moveMaze(1, 3));
 			setPietState(`${pietStanding}`);
 			moveCursor("");
-		}, 3400)
+		}, 3500)
 		return () => {
 			clearTimeout(timer0);
 			clearTimeout(timer1);
@@ -188,59 +188,27 @@ function MazeExample() {
 	);
 }
 
-function moveCursor(position: string) {
-	const cursor = document.getElementsByClassName("cursor")[0] as HTMLImageElement;
-	console.log("cursor beweegt");
-	switch (position) {
-		case "left":
-			cursor.style.left = "65vw";
-			cursor.style.top = "25vw";
-			break;
-		case "right":
-			cursor.style.left = "80vw";
-			cursor.style.top = "25vw";
-			break;
-		case "down": 
-			cursor.style.left = "72vw";
-			cursor.style.top = "30vw";
-			break;
-		default:
-			cursor.style.left = "72vw";
-			cursor.style.top = "25.3vw";
-	}
-}
-
-function moveMaze(coordinateX: number, coordinateY: number) {
-	const left = 10.5 - 78.5 * coordinateX;
-	const top = 10.5 - 78.5 * coordinateY;
-	const circleLeft = coordinateX * 20 + 10;
-	const circleTop = coordinateY * 20 + 10;
-
-	return ({
-		left: `calc( ${left}%)`,
-		top: `calc( ${top}%)`,
-		clipPath: `circle(calc(30vh / 2) at ${circleLeft}% ${circleTop}%)`
-	})
-
-}
-
-
-function Asset({ isActive, textDiewertje, sound, imgSrc, imgAlt, onShow}: AssetProps) {
+function Asset({ umpteenth, isActive, textDiewertje, sound, imgSrc, imgAlt, onShow }: AssetProps) {
 	const assetStyles = useMemo(
-		() => setAssetStyles(),
-		[]
+		() => setAssetStyles(umpteenth),
+		[umpteenth]
 	);
 	const [assetStyle, setAssetStyle] = useState(assetStyles.small);
 	const soundRef = useRef<HTMLAudioElement | null>(null);
 	const diewertjeRef = useRef<HTMLAudioElement | null>(null);
-	const bigBorder = "calc(15vh + 2px)";
+	var bigBorder: string;
+	if (isPhoneScreen) {
+		bigBorder = "calc(10vh + 2px)";
+	} else {
+		bigBorder = "calc(15vh + 2px)";
+	}
 
 	useEffect(() => {
 		if(isActive) {
 
 			const timer1 = setTimeout(() => {
 				setAssetStyle(assetStyles.big);
-				blinkSubjectBorder("3px");
+				blinkSubjectBorder("0px");
 				if(soundRef.current !== null) {
 					soundRef.current.play();
 				}
@@ -264,7 +232,7 @@ function Asset({ isActive, textDiewertje, sound, imgSrc, imgAlt, onShow}: AssetP
 				clearTimeout(timer3);
 			}
 		}
-	}, [isActive, assetStyles, textDiewertje, onShow])
+	}, [isActive, bigBorder, assetStyles, textDiewertje, onShow])
 
 	return (
 		<>
@@ -287,11 +255,68 @@ function Asset({ isActive, textDiewertje, sound, imgSrc, imgAlt, onShow}: AssetP
 }
 
 
-function setAssetStyles() {
+function moveCursor(position: string) {
+	const cursor = document.getElementsByClassName("cursor")[0] as HTMLImageElement;
+	console.log("cursor beweegt");
+	if (isPhoneScreen) {
+		switch (position) {
+			case "right":
+				cursor.style.left = "60vw";
+				cursor.style.top = "calc(58vh + 15vw)";
+				break;
+			case "down": 
+				cursor.style.left = "45vw";
+				cursor.style.top = "calc(58vh + 20vw)";
+				break;
+			default:
+				cursor.style.left = "30vw";
+				cursor.style.top = "calc(58vh + 2vw)";
+		}
+	} else {
+		switch (position) {
+			case "right":
+				cursor.style.left = "82vw";
+				cursor.style.top = "calc(38vh + 5vw)";
+				break;
+			case "down": 
+				cursor.style.left = "75vw";
+				cursor.style.top = "calc(38vh + 10vw)";
+				break;
+			default:
+				cursor.style.left = "70vw";
+				cursor.style.top = "calc(38vh + 1vw)";
+		}
+	}
+}
+
+function moveMaze(coordinateX: number, coordinateY: number) {
+	const left = isPhoneScreen ? 11 - 78.5 * coordinateX : 24 - 52.5 * coordinateX;
+	const top = isPhoneScreen ? 11 - 78.5 * coordinateY : 24 - 52.5 * coordinateY;
+	const circleLeft = coordinateX * 20 + 10;
+	const circleTop = coordinateY * 20 + 10;
+
+	return ({
+		left: `calc( ${left}%)`,
+		top: `calc( ${top}%)`,
+		clipPath: isPhoneScreen 
+			? `circle(calc(20vh / 2) at ${circleLeft}% ${circleTop}%)`
+			: `circle(calc(30vh / 2) at ${circleLeft}% ${circleTop}%)`
+	})
+
+}
+
+
+function setAssetStyles(index: number) {
+
 	const bigAssetStyle = {
-		transform: "scale(2)"
+		top: isPhoneScreen ? `calc(0.20 * ${window.innerHeight}px)` : "0px",
+		left: isPhoneScreen ? `calc(50% - 10vw)` : "0px",
+		transform: isPhoneScreen ? "scale(calc(4/3))" : "scale(2)"
 	};
 	const smallAssetStyle = {
+		left: isPhoneScreen 
+			? `calc( 0.1 * ${window.innerWidth}px + ${index} * 0.28 * ${window.innerWidth}px)` 
+			: "0px",
 		transform: "scale(1)"
 	};
 	return {big: bigAssetStyle, small: smallAssetStyle};
@@ -337,6 +362,7 @@ function calculatePositionAssetsContainer(activeIndex: number) {
 
 
 interface AssetProps {
+	umpteenth: number;
 	isActive: boolean;
 	textDiewertje: string;
 	sound: any;
