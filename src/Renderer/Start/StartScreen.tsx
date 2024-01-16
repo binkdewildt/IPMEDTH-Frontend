500f01938400e9afb4696cd766c091f07476ab0d
Dishang <dishang04@gmail.com>
2024-01-16 17:33:26 +0100

    final changes: contrast test, responsive and screen reader

--- src/Renderer/Start/StartScreen.tsx
+++ src/Renderer/Start/StartScreen.tsx
@@ -9,7 +9,7 @@ export default function StartScreen() {
 	return (
 		<section className="startScreen fullScreen">
 			{/*h1 tag voor schermlezer*/}
-			<h1 tabIndex={1} className={"hidden-header"} aria-label={"Sinterklaasjournaal doolhof"}>Sinterklaasjournaal doolhof</h1>
+			<h1 className={"hidden-header"} aria-label={"Sinterklaasjournaal doolhof"}>Sinterklaasjournaal doolhof</h1>
 			<img
 				className="sinterklaasjournaalBanner"
 				aria-hidden
@@ -17,7 +17,7 @@ export default function StartScreen() {
 				alt="Het bewegende logo van het Sinterklaasjournaal"
 				height="307"
 				width="642"></img>
-			<button tabIndex={2} aria-label="Uitleg" className="startLink" onClick={() => setExplanationOpen(true)}>
+			<button aria-label="Uitleg" className="startLink" onClick={() => setExplanationOpen(true)}>
 				<img
 					className="startImg"
 					aria-hidden
